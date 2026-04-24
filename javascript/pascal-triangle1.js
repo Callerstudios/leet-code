58747474836ca737e53072ff5f7ba9c4ec376baa
callerstudios <busariroqeeb16@gmail.com>
2026-04-24 13:24:12 +0100

    add generate function to create Pascal's triangle

---
+++ javascript/pascal-triangle1.js
@@ -0,0 +1,18 @@
+var generate = function(numRows) {
+    const res = [[1], [1, 1]]
+    if(numRows < 2){
+        return res.slice(0, numRows)
+    }
+    for(let i = 2; i < numRows; i++){
+        let newArray = [1]
+        let iterCount = i - 1;
+        for(let j = 1; j <= iterCount; j++){
+            let add1 = res[i - 1][j - 1]
+            let add2 = res[i - 1][j]
+            newArray.push(add1 + add2)
+        }
+        newArray.push(1)
+        res.push(newArray)
+    }
+    return res
+};
