01f562c6b1279bc5d8de078b25896f4749ace051
callerstudios <busariroqeeb16@gmail.com>
2026-04-25 21:57:38 +0100

    add binaryTreeSum function to calculate the sum of values in a binary tree

---
+++ javascript/xyz.js
@@ -0,0 +1,15 @@
+const xyz = (root, targetSum) => {
+    var getSum = (root, targetSum) =>{
+        if(!root){
+            return 0
+        }
+        let sum = root.val
+        if(!root.left && !root.right){
+            return sum
+        }
+        sum += getSum(root.left, targetSum) + getSum(root.right, targetSum)
+        return sum
+    }
+    console.log(getSum(root, targetSum))
+    return getSum(root, targetSum)
+}
