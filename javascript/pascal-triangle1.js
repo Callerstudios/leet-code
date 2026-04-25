1e986752c3ea82f1176d7a70a8bd5595caec9be5
callerstudios <busariroqeeb16@gmail.com>
2026-04-25 21:23:18 +0100

    add getRow function to return a specific row of Pascal's triangle

--- javascript/pascal-triangle1.js
+++ javascript/pascal-triangle1.js
@@ -16,3 +16,20 @@ var generate = function(numRows) {
     }
     return res
 };
+
+var getRow = function(rowIndex) {
+    let numRows = rowIndex + 1
+    const res = [[1], [1, 1]]
+    for(let i = 2; i < numRows; i++){
+        let newArray = [1]
+        let iterCount = i - 1;
+        for(let j = 1; j <= iterCount; j++){
+            let add1 = res[i - 1][j - 1]
+            let add2 = res[i - 1][j]
+            newArray.push(add1 + add2)
+        }
+        newArray.push(1)
+        res.push(newArray)
+    }
+    return res[rowIndex]
+};
