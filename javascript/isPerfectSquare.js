b0410615b7854763657d5715c7294058bb3c803a
callerstudios <busariroqeeb16@gmail.com>
2026-04-28 10:22:23 +0100

    add binary search implementation to isPerfectSquare function for improved efficiency

--- javascript/isPerfectSquare.js
+++ javascript/isPerfectSquare.js
@@ -3,3 +3,25 @@ var isPerfectSquare = function (num) {
   let floored = Math.floor(sqrt);
   return sqrt === floored;
 };
+var isPerfectSquare = function (num) {
+  let min = 2;
+  let max = Math.floor(num / 2);
+  if (num === 1) {
+    return true;
+  }
+
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
+    let sqrd = mid * mid;
+    if (sqrd === num) {
+      return true;
+    } else if (sqrd < num) {
+      min = mid;
+      min += 1;
+    } else if (sqrd > num) {
+      max = mid;
+      max -= 1;
+    }
+  }
+  return false;
+};
