15f7c40759cfd84005b2c9a285fbd5d992c04d8f
callerstudios <busariroqeeb16@gmail.com>
2026-04-30 22:43:06 +0100

    refactor containsDuplicate function for clarity and consistency

--- javascript/containsDuplicate.js
+++ javascript/containsDuplicate.js
@@ -1,4 +1,14 @@
 var containsDuplicate = function (nums) {
   let setRep = new Set(nums);
-  return [...setRep].length != nums.length;
+  return setRep.size != nums.length;
+};
+var containsDuplicate = function (nums) {
+  let seen = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (seen[nums[i]]) {
+      return true;
+    }
+    seen[nums[i]] = "yes";
+  }
+  return false;
 };
